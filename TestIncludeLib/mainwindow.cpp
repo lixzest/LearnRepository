2b5224a9a28bbb6c1c42106c90414029933a6991
lixzest <42462439+lixzest@users.noreply.github.com>
2020-09-15 19:51:57 +0800

    Add files via upload
    qt  call dynamic library

---
+++ TestIncludeLib/mainwindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    TestLoaderLib *mTest = new TestLoaderLib;
+    qDebug() << mTest->Test("hello");
+
+    connect(mTest,SIGNAL(sendInfo(QString)),this,SLOT(getInfo(QString)));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::getInfo(QString info)
+{
+    qDebug() << "11111" << info;
+}
---
+++ TestIncludeLib/mainwindow.h
@@ -0,0 +1,25 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "TestLoaderLib.h"
+#include "testloaderlib_global.h"
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+public slots:
+    void getInfo(QString info);
+private:
+    Ui::MainWindow *ui;
+    TestLoaderLib *mTest;
+};
+
+#endif // MAINWINDOW_H
